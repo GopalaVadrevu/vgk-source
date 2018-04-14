27fdebee1fe42e8dd39840bad997112f0496c55c
gopalavadrevu <gkvadrevu@gmail.com>
2018-04-14 22:14:44 +0530

    ciommit

---
+++ FrontStack/exercise5.js
@@ -0,0 +1,58 @@
+// Complete the below questions using this array:
+const array = [
+  {
+    username: "john",
+    team: "red",
+    score: 5,
+    items: ["ball", "book", "pen"]
+  },
+  {
+    username: "becky",
+    team: "blue",
+    score: 10,
+    items: ["tape", "backpack", "pen"]
+  },
+  {
+    username: "susy",
+    team: "red",
+    score: 55,
+    items: ["ball", "eraser", "pen"]
+  },
+  {
+    username: "tyson",
+    team: "green",
+    score: 1,
+    items: ["book", "pen"]
+  },
+
+];
+
+//Create an array using forEach that has all the usernames with a "!" to each of the usernames
+const myarr=[];
+const nameArr=array.forEach(element=>{myarr.push(element.username+"!");});
+
+//Create an array using map that has all the usernames with a "? to each of the usernames
+const mapArr = array.map(element=>element.username+"?");
+console.log("maparr",mapArr);
+
+//Filter the array to only include users who are on team: red
+const redArr=array.filter(element=>element.team==="red");
+console.log("redarr",redArr);
+
+//Find out the total score of all users using reduce
+const scoreArr =array.reduce((acc,element)=>{return acc+element.score;},0);
+console.log("scoreArr",scoreArr);
+
+// (1), what is the value of i?
+// (2), Make this map function pure:
+const arrayNum = [1, 2, 4, 5, 8, 9];
+const newArray = arrayNum.map((num, i) => {
+	console.log(num, i);
+	alert(num);
+	return num * 2;
+})
+const mpArr = arrayNum.map(num => num * 2);
+console.log("mpArr",mpArr);
+//BONUS: create a new list with all user information, but add "!" to the end of each items they own.
+const newLists1=array.map(element => [{"username":element.username,"team":element.team,"score":element.score,"items":element.items.map(it => it+"!")}]);
+console.log("newList", newLists);
